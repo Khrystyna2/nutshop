650f1171695a887df8069885509000811e10d333
Khrystyna Khozhai <l.xrustja@gmail.com>
2018-07-30 07:48:25 +0300

    adaptive page for tablet and mobil

--- js/main-js.js
+++ js/main-js.js
@@ -7,11 +7,52 @@ $(document).ready(function() {
 
 
 
-	$('.slick').slick({
+	$('.slider-catalog').slick({
 		arrows: true,
-		infinite: true,
+		// infinite: true,
+		// mobileFirst: true,
 		slidesToShow: 4,
-		slidesToScroll: 2
+		slidesToScroll: 4,
+		speed: 1000,
+		// autoplay:true,
+        // mobileFirst:true,
+// 		centerMode: true,
+		// variableWidth: true,
+		responsive: [
+			{
+			  breakpoint: 960,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				// infinite: true,
+				// centerMode: true
+			  }
+			},
+			{
+			  breakpoint: 640,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				// centerMode: true
+			  }
+			},
+			{
+			  breakpoint: 450,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				// centerMode: true
+			  }
+			}
+		  ]
   	
 	});
+
+
+	$('.menu-icon').click(function(){
+		$(this).toggleClass('open');
+		$(this).next('ul.menu').toggleClass('open');
+	});
+
+	
 });
